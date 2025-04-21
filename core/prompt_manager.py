18ddee45dd00f466ae52838a91786b86d880c440
Faizal Nursaori <faizalrahmatn@gmail.com>
2025-04-21 14:30:36 +0700

    feat: add tavily

--- core/prompt_manager.py
+++ core/prompt_manager.py
@@ -2,11 +2,12 @@ from openai import OpenAI
 
 openai_client = OpenAI()
 
+
 class PromptManager:
-    def __init__(self, model = "gpt-4o", messages = []):
+    def __init__(self, model="gpt-4o", messages=[]):
         self.model = model
         self.messages = messages
-        
+
     def add_message(self, role, content):
         self.messages.append({"role": role, "content": content})
 
@@ -15,18 +16,15 @@ class PromptManager:
 
     def generate(self):
         response = openai_client.chat.completions.create(
-            model = self.model,
-            messages=self.messages
+            model=self.model, messages=self.messages
         )
 
         content = response.choices[0].message.content
         return content
-    
+
     def generate_structured(self, schema):
         response = openai_client.beta.chat.completions.parse(
-            model = self.model,
-            messages = self.messages,
-            response_format = schema
+            model=self.model, messages=self.messages, response_format=schema
         )
 
         content = response.choices[0].message.parsed
--- researcher/views.py
+++ researcher/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.views.generic import View
+from researcher.methods import generate_queries, research, generate_business_profile
+
 
 # Create your views here.
+class IndexView(View):
+    def get(self, request):
+        return render(request, "index.html")
+    
+    def post(self, request):
+        company_name = request.POST.get("company_name")
+
+        queries = []
+
+        financial_queries = generate_queries("Finance", company_name)
+        leadership_queries = generate_queries("Leadership", company_name)
+        operations_queries = generate_queries("Operations", company_name)
+        company_history_queries = generate_queries("Company History", company_name)
+
+        queries.extend(financial_queries.get("queries"))
+        queries.extend(leadership_queries.get("queries"))
+        queries.extend(operations_queries.get("queries"))
+        queries.extend(company_history_queries.get("queries"))
+
+        context = ""
+
+        for query in queries[:3]:
+            response = research(query)
+            context += f"Query: {query} \nResponse: {response} \n\n"
+
+        # print(context)
+        business_profile = generate_business_profile(context)
+        print(business_profile)
+
+        return render(request, "index.html")
